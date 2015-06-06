eb9b5483993f63d0afb2326c20296ca510bce090
Imsun <mBusyCoding@gmail.com>
2015-06-07 02:11:35 +0800

    hide foo

--- webrtc_video/static/js/room_new.app.js
+++ webrtc_video/static/js/room_new.app.js
@@ -45,6 +45,7 @@ define(['jquery', 'file_meta', 'p2p', 'utils', 'underscore'], function($, file_m
           client.piece_queue = [];
           client.finished_piece = _.map(client.finished_piece, function() { return 1; });
           client.update_bitmap();
+          if (parent) parent.window.setHash(file_meta.hash)
           J_console.append('<li>room created: <a href="/room/'+file_meta.hash+'" target=_blank>'+
                            location.href.replace(/room\/new.*$/i, 'room/'+file_meta.hash)+'</a>');
           J_console.append('<li><dl class=info>'+
